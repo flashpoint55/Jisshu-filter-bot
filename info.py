93ed691dd3c172d423a32de72333a75f302dda32
flashpoint55 <koyeb0125@gmail.com>
2024-11-06 11:40:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,14 +15,14 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '23378704'))
 API_HASH = environ.get('API_HASH', '15a02b4d02babeb79e8f328b0ead0c17')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7936662852:AAGS_u4jtDunV9p-5j_cWJG-LMPurV51gDk')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7324621271:AAF5SxxGyAGsVgwSaBZU01xkiy-XNYyH3Zs')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7364818327').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Patrick_Bateman_r") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002149281691'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002445234549'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/avengerdoomsday1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002285348355').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002319221632').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://koyeb0125:NISQEh3836CQLw2h@cluster0.onkp2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
@@ -69,7 +69,7 @@ REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://graph.org/file/7993e069f4b053e14e752-b3739dd19735658768.jpg')).split()
+START_IMG = (environ.get('START_IMG', 'https://graph.org/file/1d3f629d4343f3497b481-00c92737d64ae4d0c0.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/940a66d73b9a38a7e9045-b653c4c40d4d76b3cd.jpg')).split()
